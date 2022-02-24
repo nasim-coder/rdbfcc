3f9b34ffa96dfa6df052ad81ffdccc8e11fdd0dc
NakRex <khannasimahmad@gmail.com>
2022-02-25 00:00:37 +0530

    created user model

---
+++ model/bookStock.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+let bookStockSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    },
+    bookname: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    isbn: [{
+        type: Number,
+        required: true,
+        unique: true
+    }],
+});
+
+let BookStock = mongoose.model('BookStock', bookStockSchema);
+module.exports = BookStock;
--- model/user.js
+++ model/user.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+let User = mongoose.model('User', userSchema);
+module.exports = User;

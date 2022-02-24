236b80fd7297e68045c5ed0dc19a7297d6dd111e
NakRex <khannasimahmad@gmail.com>
2022-02-25 00:27:08 +0530

    added user controller

---
+++ controller/userController.js
@@ -0,0 +1,40 @@
+//user register
+exports.register = (req, res) => {
+    
+}
+
+//user login
+exports.login = (req, res) => {
+
+};
+
+//create store if it deoesnt exist
+exports.createStore = (req, res) => {
+    
+};
+
+//add stock in the store if the user has a store and same stock is not available in his store
+exports.addStock = (req, res) => {
+    
+};
+
+//user add book in the exact stock if store and stock available and if the user is valid.
+exports.updateStock = (req, res) => {
+    
+};
+
+//valid store and stock holder delte a book
+exports.deleteBookfromStock = (req, res) => {
+    
+};
+
+//a normal user can see all the stores irrespective of the owner
+exports.viewStores = (req, res) => {
+    
+}
+
+//a normal user can see the available books in the store irrespective of the owner
+exports.viewStock = (req, res) => {
+    
+}
+

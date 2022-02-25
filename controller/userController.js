675dc8caeebaa984c4e2e05218cb8d6e04b7aa5b
NakRex <khannasimahmad@gmail.com>
2022-02-25 21:14:55 +0530

    added middle ware and reister function

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,51 @@
+const User = require('../model/user')
+const bcrypt = require('bcryptjs');
+const jwt = require('json-web-token');
+
+
 //user register
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
+
+    const { name, email, password } = req.body;
     
-}
+    let isAlreadyexist = await User.findOne({ email });
 
-//user login
-exports.login = (req, res) => {
-
+    if (!isAlreadyexist) {
+        let user = new User({
+            name: name,
+            email: email,
+            password: bcrypt.hashSync(password, 8)
+        });
+        user.save((err, user) => {
+            if (err) {
+                return res.status(500).send({ success: false, message: err })
+            
+            }
+            return res.status(200).json('you are registered successfully')
+        });
+    } else {
+        return res.status(501).send({msg: 'user already exist', success: false});
+    }
+        
 };
+
+
+//login function for the user and assigning token
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+        res.status(400).send("All input is required");
+    }
+    const user = await User.findOne({ email });
+    let isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (user && (isPasswordCorrect)) {
+        const token = jwt.sign({ user }, jwtconfig.secret, { "expiresIn": "2h" });
+        user.token = token;
+        res.status(200).json(user)
+    } else {
+        res.status(400).send("invalid credential")
+    }
+}
 
 //create store if it deoesnt exist
 exports.createStore = (req, res) => {
